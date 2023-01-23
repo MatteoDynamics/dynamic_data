8221c37ca5975c748edffe10c8684fdd8f58721d
MatteoDynamics <matteotennisboy@gmail.com>
2023-01-23 23:36:56 +0100

    ended project

--- code.c
+++ code.c
@@ -80,7 +80,6 @@ void clear_list(node** lista) {
 
 void read_from_csv(node **lista)
 {
-    // Open the CSV file
     FILE *file = fopen("prosta_lista (1).csv", "r");
     if(file == NULL)
     {
@@ -88,7 +87,6 @@ void read_from_csv(node **lista)
         return;
     }
 
-    // Read each line of the file
     char line[1024];
     while (fgets(line, 1024, file))
     {
@@ -112,8 +110,6 @@ void read_from_csv(node **lista)
         // Add the 'dane' struct to the linked list
         add(lista, dane);
     }
-
-    // Close the file
     fclose(file);
 }
 
@@ -194,7 +190,7 @@ void sort_list(node **lista, char field[20]) {
                 swapped = 1;
             }
         } else if (strcmp(field, "group") == 0) {
-char group1[20], group2[20];
+            char group1[20], group2[20];
             if(strlen(ptr1->data.group)>=19) 
                 strncpy(group1, ptr1->data.group,19);
             else
@@ -222,72 +218,8 @@ char group1[20], group2[20];
   } while (swapped);
 }
 
-void add_sorted_input(node **lista, char field[20]) {
-    if(strcmp(field, "name") != 0 && strcmp(field, "surname") != 0 && strcmp(field, "group") != 0) {
-        printf("Error: Invalid field.\n");
-        return;
-    }
-    *lista = get_first(*lista);
-
-    kontakt dane;
-
-    printf("Enter contact name: ");
-    scanf("%s", dane.name);
-
-    printf("Enter contact surname: ");
-    scanf("%s", dane.surname);
-
-    printf("Enter contact number: ");
-    scanf("%s", dane.number);
-
-    printf("Enter contact group: ");
-    scanf("%s", dane.group);
-
-    node *new = malloc(sizeof(node));
-    new->data = dane;
-    new->prev = NULL;
-    new->next = NULL;
-
-    node *current = *lista;
-    node *prev = NULL;
-    
-    // change the while loop to traverse the list until it reaches the correct position to insert the new contact
-    while (current != NULL) {
-        int comparison;
-        if(strcmp(field, "name") == 0) {comparison = strcmp(dane.name, current->data.name);} else if (strcmp(field, "surname") == 0) {comparison = strcmp(dane.surname, current->data.surname);} else if (strcmp(field, "group") == 0) {comparison = strcmp(dane.group, current->data.group);}
-if(comparison < 0) {
-break;
-}
-prev = current;
-current = current->next;
-}
-// check if the new contact should be inserted at the beginning of the list
-if (prev == NULL && *lista != NULL) {
-    new->next = *lista;
-    (*lista)->prev = new;
-    *lista = new;
-}
-// check if the new contact should be inserted at the end of the list
-else if (current == NULL) {
-    if(prev != NULL) {
-        new->prev = prev;
-        prev->next = new;
-    } else {
-        *lista = new;
-    }
-}
-// insert the new contact in the middle of the list
-else {
-    new->prev = prev;
-    new->next = current;
-    prev->next = new;
-    current->prev = new;
-}
-}
-
 void search_contacts(node *lista, char search[20])
 {
-    // get the first element of the linked list
     node *current = get_first(lista);
     int i;
     // convert the search query to lowercase
@@ -306,10 +238,10 @@ void search_contacts(node *lista, char search[20])
         for(i = 0; i < strlen(surname_temp); i++) {
             surname_temp[i] = tolower(surname_temp[i]);
         }
-        // check if the name or surname field contains the search query
+        
         if (strstr(name_temp, search) != NULL || strstr(surname_temp, search) != NULL)
         {
-            // print out the name and surname fields of the contact
+            
             printf("name: %-20s surname: %-20s\n", current->data.name, current->data.surname);
         }
         current = current->next;
@@ -318,16 +250,16 @@ void search_contacts(node *lista, char search[20])
 
 void search_group(node *lista, char group[20])
 {
-    // get the first element of the linked list
+    
     node *current = get_first(lista);
     
     for(int i = 0; i < strlen(group); i++) {
             group[i] = tolower(group[i]);
         }
-    // iterate through the linked list
+    
     while (current != NULL)
     {
-        // check if the group field matches the search query
+       
         char group_temp[20];
         strcpy(group_temp,current->data.group);
         for(int i = 0; i < strlen(group_temp); i++) {
@@ -335,70 +267,223 @@ void search_group(node *lista, char group[20])
         }
         if (strstr(group_temp, group) != 0)
         {
-            // print out the name, surname and number fields of the contact
+            
             printf("\ found:    name: %-20s surname: %-20s number: %-20s\n", current->data.name, current->data.surname, current->data.number);
         }
         current = current->next;
     }
 }
 
-void remove_contact(node **lista, char name[20], char surname[20]) {
-    node *current = get_first(*lista);
+void delete_contact(node **lista, char name[20], char surname[20]) {
+    node *current = get_first(*lista); // get the first node in the list
     while (current != NULL) {
-        char current_name[20], current_surname[20];
-        strcpy(current_name, current->data.name);
-        strcpy(current_surname, current->data.surname);
-        for(int i = 0; current_name[i]; i++){
-            current_name[i] = tolower(current_name[i]);
-        }
-        for(int i = 0; current_surname[i]; i++){
-            current_surname[i] = tolower(current_surname[i]);
-        }
-        if (strcmp(current_name, name) == 0 && strcmp(current_surname, surname) == 0) {
-            // Contact found, remove it from the list
+        if (strcmp(current->data.name, name) == 0 && strcmp(current->data.surname, surname) == 0) {
             if (current->prev != NULL) {
                 current->prev->next = current->next;
             }
             if (current->next != NULL) {
                 current->next->prev = current->prev;
             }
-            if (current == *lista) {
-                *lista = current->next;
-            }
             free(current);
+            printf("\nContact %s %s deleted\n", name, surname);
             return;
         }
         current = current->next;
     }
-    // Contact not found
-    printf("Error: Contact not found.\n");
+    printf("Error: \nContact %s %s not found\n", name, surname);
+}
+
+kontakt create_contact() {
+    kontakt new_contact;
+
+    printf("Enter name: ");
+    scanf("%s", new_contact.name);
+    printf("Enter surname: ");
+    scanf("%s", new_contact.surname);
+    printf("Enter phone number: ");
+    scanf("%s", new_contact.number);
+    printf("Enter group: ");
+    scanf("%s", new_contact.group);
+
+    return new_contact;
 }
 
 
+void add_sorted(node **lista, kontakt dane, char field[20]) {
+    if (*lista == NULL) {
+        *lista = malloc(sizeof(node));
+        if (lista == NULL) 
+        {
+        free_list(*lista);
+        exit(1);
+        }
+        (*lista)->data = dane;
+        (*lista)->prev = NULL;
+        (*lista)->next = NULL;
+        return;
+    }
+    node *current = get_first(*lista);
+    if(strcmp(field, "name") == 0) {
+        while (current->next != NULL && strcmp(current->next->data.name, dane.name) < 0) {
+            current = current->next;
+        }
+    } else if(strcmp(field, "surname") == 0) {
+        while (current->next != NULL && strcmp(current->next->data.surname, dane.surname) < 0) {
+            current = current->next;
+        }
+    }
+    node *new = malloc(sizeof(node));
+    if (new == NULL) 
+        {
+        free_list(*lista);
+        exit(1);
+        }
+    new->data = dane;
+    new->prev = current;
+    new->next = current->next;
+    if (current->next != NULL) {
+        current->next->prev = new;
+    }
+    current->next = new;
+}
+
+void menu_option(int * menu1)
+{
+    printf("Type what you want to do?\n");
+    printf("\n1)sort by name\n2)sort by surname\n3)sort by group\n");
+    while (1) {
+    printf("type probability to noise: ");
+    if (scanf("%d", &*menu1) == 1) {
+      break;
+    } else {
+      printf("Invalid input, please try again.\n");
+      while (getchar() != '\n');
+    }
+  }
+}
+
+void menu_option3(int * menu3)
+{
+    printf("Type what you want to do?\n");
+    printf("\n1)find by name and surname\n2)find by group\n");
+    while (1) {
+    printf("type probability to noise: ");
+    if (scanf("%d", &*menu3) == 1) {
+      break;
+    } else {
+      printf("Invalid input, please try again.\n");
+      while (getchar() != '\n');
+    }
+  }
+}
+
+void main_menu(int * menu)
+{
+    printf("Type what you want to do?\n");
+    printf("\n1)sort\n2)add\n3)searsch\n4)delete\n5)quit\n");
+    while (1) {
+    printf("type probability to noise: ");
+    if (scanf("%d", &*menu) == 1) {
+      break;
+    } else {
+      printf("Invalid input, please try again.\n");
+      while (getchar() != '\n');
+    }
+  }
+}
 
 int main()
-{node *lista = NULL;kontakt dane;char name_delete[20]; char surname_delete[20];
+{node *lista = NULL;kontakt dane;int menu1;int menu;int menu3;char name_delete[20], surname_d[20];
     char name[20];
     char surname[20];
-    //printf("Enter name to search: ");
-    //scanf("%s", name);
-
-
     read_from_csv(&lista);
-    //search_contacts(lista, name);
-    printf("Enter group to search: \n");
-    scanf("%s", name);
-    search_group(lista, name);
-    //add(&lista,dane);
-    //add(&lista,dane);
-    sort_list(&lista, "surname");
     print_list(lista);
-//add_sorted_input(&lista, "surname");
-//print_list(lista);
-    printf("type name and surname to delete contact");
-    scanf("%s",&name_delete);
-    scanf("%s",&surname_delete);
-    remove_contact(&lista, name_delete,surname_delete);
-    print_list(lista);
-    clear_list(&lista);
+    do
+    {
+        main_menu(&menu);
+        switch(menu)
+        {
+            //SORT
+            case 1:
+            {
+
+    menu_option(&menu1);
+    switch(menu1)
+    {
+        case 1:
+        {
+            sort_list(&lista, "name");
+            print_list(lista);
+        }break;
+        case 2:
+        {
+            sort_list(&lista, "surname");
+            print_list(lista);
+        }break;
+        case 3:
+        {
+            sort_list(&lista, "group");
+            print_list(lista);
+        }break;
+
+    }
+    
+            }break;
+            //ADD
+            case 2:
+            {
+                kontakt new_contact = create_contact();
+                add_sorted(&lista, new_contact, "surname");
+                print_list(lista);
+            }break;
+            //FIND
+            case 3:
+            {
+                menu_option3(&menu3);
+                switch(menu3)
+                {
+                    case 1:
+                    {
+                        printf("Enter name to search: \n");
+                scanf("%s", name);
+                        search_contacts(lista, name);
+                    }break;
+
+                    case 2:
+                    {
+                        printf("Enter surname to search: \n");
+                scanf("%s", name);
+                        search_contacts(lista, name);
+                    }break;
+                    case 3:
+                    {
+                        printf("Enter group to search: \n");
+                scanf("%s", name);
+                        search_group(lista, name);
+                    }
+                }
+                
+                
+            }break;
+            //DELETE
+            case 4:
+            {
+                printf("\nEnter the name of the contact to be deleted: ");
+                scanf("%s", name_delete);
+                printf("Enter the surname of the contact to be deleted: ");
+                scanf("%s", surname_d);
+                delete_contact(&lista, name_delete, surname_d);
+            }break;
+            //Quitting;
+            case 5:
+            {
+                printf("QUITTING!");
+                clear_list(&lista);
+                printf("Free memory!");
+                exit(0);
+            }
+        }
+    } while (menu<=5&&menu>=1);
+
+    return 0;
 }
